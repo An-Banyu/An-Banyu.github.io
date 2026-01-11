88e0dc759803cc1d04aef91161edba7fe47c134b
An-Banyu <3067937971@qq.com>
2026-01-11 12:26:46 +0800

    Site updated: 2026-01-11 12:26:33

--- js/loading.js
+++ js/loading.js
@@ -2,14 +2,18 @@
   // 假设你把 SVG 内容保存为了 source/loading.svg
   
   const loaderHTML = `
-    <div id="loader-wrapper">
-        <div class="loader-bg"></div>
-        <div class="loader-content">
-            <div id="svg-container" class="loading-svg">
-               </div>
-            <div style="margin-top:20px; color: #666; font-family: sans-serif;">Loading...</div>
-        </div>
-    </div>
+  <div id="loader-wrapper">
+          <div class="loader-bg"></div>
+          <div class="loader-content">
+              <div id="svg-container" class="loading-svg">
+                </div>
+              
+              <div class="loading-text">
+                  欢迎光临不夜天<br>
+                  loading...
+              </div>
+              </div>
+      </div>
   `;
 
   document.body.insertAdjacentHTML('afterbegin', loaderHTML);
@@ -43,11 +47,10 @@
             [indices[i], indices[j]] = [indices[j], indices[i]];
         }
 
-        // 慢慢显示
         let i = 0;
         const interval = setInterval(() => {
-            // 每次显示 20 个碎片，加快速度
-            for(let k=0; k<20; k++) {
+            // 每次显示 50 个碎片
+            for(let k=0; k<50; k++) {
                 if(i >= totalPaths) {
                     clearInterval(interval);
                     break;
@@ -68,18 +71,21 @@
       // 稍微多等一会儿，让拼图拼完
       setTimeout(function() {
           loader.classList.add('loaded');
-      }, 3000); 
+          document.body.classList.add('loaded');
+      }, 2400); // 根据碎片显示速度调整这个时间
   });
 
   // PJAX 适配
   document.addEventListener('pjax:send', function () {
       loader.classList.remove('loaded');
+      document.body.classList.remove('loaded');
       loader.classList.add('loading');
   });
   document.addEventListener('pjax:complete', function () {
       setTimeout(function() {
           loader.classList.remove('loading');
           loader.classList.add('loaded');
+          document.body.classList.add('loaded');
       }, 1000);
   });
 
